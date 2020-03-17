f380842dffe7c2cc0c7fe2c6ea4e74c829c201ab
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-03-16 22:45:11 -0700

    Get HTraversable to compile

--- hedgehog/src/Hedgehog/Internal/HTraversable.purs
+++ hedgehog/src/Hedgehog/Internal/HTraversable.purs
@@ -1,11 +1,13 @@
-module Hedgehog.Internal.HTraversable (
-    HTraversable(..)
+module Hedgehog.Internal.HTraversable
+  ( class HTraversable
+  , htraverse  
   ) where
 
+import Prelude
 
 -- | Higher-order traversable functors.
 --
 -- This is used internally to make symbolic variables concrete given an 'Environment'.
 --
 class HTraversable t where
-  htraverse :: Applicative f => (forall a. g a -> f (h a)) -> t g -> f (t h)
+  htraverse :: forall f g h. Applicative f => (forall a. g a -> f (h a)) -> t g -> f (t h)
