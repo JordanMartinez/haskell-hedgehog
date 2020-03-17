ac864821f477f6ea5bfd9f9c70d6b994e33ba7af
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-03-16 22:47:32 -0700

    Get Opaque to compile

--- hedgehog/src/Hedgehog/Internal/Opaque.purs
+++ hedgehog/src/Hedgehog/Internal/Opaque.purs
@@ -2,26 +2,23 @@ module Hedgehog.Internal.Opaque (
     Opaque(..)
   ) where
 
+import Prelude
 
 -- | Opaque values.
---
---   Useful if you want to put something without a 'Show' instance inside
---   something which you'd like to be able to display.
---
---   For example:
---
--- @
---   data State v =
---     State {
---         stateRefs :: [Var (Opaque (IORef Int)) v]
---       } deriving (Eq, Show)
--- @
---
-newtype Opaque a =
-  Opaque {
-      unOpaque :: a
-    } deriving (Eq, Ord)
+-- |
+-- | Useful if you want to put something without a 'Show' instance inside
+-- | something which you'd like to be able to display.
+-- |
+-- | For example:
+-- |
+-- | ```
+-- | type State v =
+-- |    { stateRefs :: [Var (Opaque (Ref Int)) v] }
+-- | ```
+newtype Opaque a = Opaque a
 
-instance Show (Opaque a) where
-  showsPrec _ (Opaque _) =
-    showString "Opaque"
+derive newtype instance eqOpaque :: Eq a => Eq (Opaque a)
+derive newtype instance ordOpaque :: Ord a => Ord (Opaque a)
+
+instance showOpaque :: Show (Opaque a) where
+  show (Opaque _) = "Opaque"
