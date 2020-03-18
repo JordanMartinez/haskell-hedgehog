a0f6b93a6bee7051ff04f7537d344501c5103579
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-03-17 21:04:28 -0700

    Update Source to almost compile on PS (GHC Stack api; numeric TC)

--- hedgehog/src/Hedgehog/Internal/Source.purs
+++ hedgehog/src/Hedgehog/Internal/Source.purs
@@ -11,67 +11,64 @@ module Hedgehog.Internal.Source (
   , withFrozenCallStack
   ) where
 
+-- TODO: determine how to convert this API into PureScript API
 import GHC.Stack (CallStack, HasCallStack, SrcLoc(..))
 import GHC.Stack (callStack, getCallStack, withFrozenCallStack)
 
+newtype LineNo = LineNo Int
+derive newtype instance eqLineNo :: Eq LineNo
+derive newtype instance ordLineNo :: Ord LineNo
+derive newtype instance enumLineNo :: Enum LineNo
+-- TODO: Num, Real, and Integral instances
 
-newtype LineNo =
-  LineNo {
-      unLineNo :: Int
-    } deriving (Eq, Ord, Num, Enum, Real, Integral)
+newtype ColumnNo = ColumnNo Int
+derive newtype instance eqColumnNo :: Eq ColumnNo
+derive newtype instance ordColumnNo :: Ord ColumnNo
+derive newtype instance enumColumnNo :: Enum ColumnNo
+-- TODO: Num, Real, and Integral instances
 
-newtype ColumnNo =
-  ColumnNo {
-      unColumnNo :: Int
-    } deriving (Eq, Ord, Num, Enum, Real, Integral)
-
-data Span =
-  Span {
-      spanFile :: !FilePath
-    , spanStartLine :: !LineNo
-    , spanStartColumn :: !ColumnNo
-    , spanEndLine :: !LineNo
-    , spanEndColumn :: !ColumnNo
-    } deriving (Eq, Ord)
+type Span =
+  { file :: FilePath
+  , startLine :: LineNo
+  , startColumn :: ColumnNo
+  , endLine :: LineNo
+  , endColumn :: ColumnNo
+  }
 
 getCaller :: CallStack -> Maybe Span
 getCaller stack =
   case getCallStack stack of
-    [] ->
+    Nil ->
       Nothing
-    (_, x) : _ ->
-      Just $ Span
-        (srcLocFile x)
-        (fromIntegral $ srcLocStartLine x)
-        (fromIntegral $ srcLocStartCol x)
-        (fromIntegral $ srcLocEndLine x)
-        (fromIntegral $ srcLocEndCol x)
+    Cons _ (Cons x _) ->
+      Just
+        { file: srcLocFile x
+        , startLine: fromIntegral $ srcLocStartLine x
+        , startColumn: fromIntegral $ srcLocStartCol x
+        , endLine: fromIntegral $ srcLocEndLine x
+        , endColumn: fromIntegral $ srcLocEndCol x
+        }
 
 ------------------------------------------------------------------------
 -- Show instances
 
-instance Show Span where
-  showsPrec p (Span file sl sc el ec) =
-    showParen (p > 10) $
-      showString "Span " .
-      showsPrec 11 file .
-      showChar ' ' .
-      showsPrec 11 sl .
-      showChar ' ' .
-      showsPrec 11 sc .
-      showChar ' ' .
-      showsPrec 11 el .
-      showChar ' ' .
-      showsPrec 11 ec
+-- Jordan's note: Show instance isn't needed as Record has Show instance already
+-- instance Show Span where
+--   showsPrec p (Span file sl sc el ec) =
+--     showParen (p > 10) $
+--       showString "Span " .
+--       showsPrec 11 file .
+--       showChar ' ' .
+--       showsPrec 11 sl .
+--       showChar ' ' .
+--       showsPrec 11 sc .
+--       showChar ' ' .
+--       showsPrec 11 el .
+--       showChar ' ' .
+--       showsPrec 11 ec
 
-instance Show LineNo where
-  showsPrec p (LineNo x) =
-    showParen (p > 10) $
-      showString "LineNo " .
-      showsPrec 11 x
+instance showLineNo :: Show LineNo where
+  show (LineNo x) = "LineNo(" <> show x <> ")"
 
-instance Show ColumnNo where
-  showsPrec p (ColumnNo x) =
-    showParen (p > 10) $
-      showString "ColumnNo " .
-      showsPrec 11 x
+instance showColumnNo :: Show ColumnNo where
+  show (ColumnNo x) = "ColumnNo(" <> show x <> ")"
