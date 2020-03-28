ab300033b8fd10c50883b92719f93c01d437d0a0
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-03-27 18:39:42 -0700

    Update Queue to PS syntax

--- hedgehog/src/Hedgehog/Internal/Queue.purs
+++ hedgehog/src/Hedgehog/Internal/Queue.purs
@@ -16,7 +16,7 @@ import           Control.Concurrent.Async (forConcurrently)
 import           Control.Concurrent.MVar (MVar)
 -- import qualified Control.Concurrent.MVar as MVar
 import           Control.Monad (when)
-import           Control.Monad.IO.Class (MonadIO(..))
+import           Effect.Aff.Class (MonadAff(..))
 
 import           Data.Map.Strict (Map)
 -- import qualified Data.Map.Strict as Map
@@ -26,36 +26,37 @@ import           Data.Map.Strict (Map)
 import           Hedgehog.Internal.Config
 
 
-newtype TaskIndex =
-  TaskIndex Int
-  deriving (Eq, Ord, Enum, Num)
+newtype TaskIndex = TaskIndex Int
+derive newtype instance eqTaskIndex :: Eq TaskIndex
+derive newtype instance ordTaskIndex :: Ord TaskIndex
+derive newtype instance enumTaskIndex :: Enum TaskIndex
+-- TODO: Num instance conversion
 
-newtype TasksRemaining =
-  TasksRemaining Int
+newtype TasksRemaining = TasksRemaining Int
 
-dequeueMVar ::
-     MVar [(TaskIndex, a)]
-  -> (TasksRemaining -> TaskIndex -> a -> IO b)
-  -> IO (Maybe (TaskIndex, b))
+dequeueMVar :: forall a b.
+     AVar (List (Tuple TaskIndex a))
+  -> (TasksRemaining -> TaskIndex -> a -> Aff b)
+  -> Aff (Maybe (Tuple TaskIndex b))
 dequeueMVar mvar start =
   MVar.modifyMVar mvar $ case _ of
-    [] ->
-      pure ([], Nothing)
-    (ix, x) : xs -> do
+    Nil ->
+      pure (Tuple Nil Nothing)
+    Cons (Tuple ix x) xs -> do
       y <- start (TasksRemaining $ length xs) ix x
-      pure (xs, Just (ix, y))
+      pure (Tuple xs (Just (Tuple ix y)))
 
-runTasks ::
+runTasks :: forall a b c.
      WorkerCount
-  -> [a]
-  -> (TasksRemaining -> TaskIndex -> a -> IO b)
-  -> (b -> IO ())
-  -> (b -> IO ())
-  -> (b -> IO c)
-  -> IO [c]
+  -> List a
+  -> (TasksRemaining -> TaskIndex -> a -> Aff b)
+  -> (b -> Aff Unit)
+  -> (b -> Aff Unit)
+  -> (b -> Aff c)
+  -> Aff (List c)
 runTasks n tasks start finish finalize runTask = do
   qvar <- MVar.newMVar (zip [0..] tasks)
-  fvar <- MVar.newMVar (-1, Map.empty)
+  fvar <- MVar.newMVar (Tuple -1 Map.empty)
 
   let
     worker rs = do
@@ -63,7 +64,7 @@ runTasks n tasks start finish finalize runTask = do
       case mx of
         Nothing ->
           pure rs
-        Just (ix, x) -> do
+        Just (Tuple ix x) -> do
           r <- runTask x
           finish x
           finalizeTask fvar ix (finalize x)
@@ -73,43 +74,43 @@ runTasks n tasks start finish finalize runTask = do
   fmap concat <<< forConcurrently [1..max 1 n] $ \_ix ->
     worker []
 
-runActiveFinalizers ::
-     MonadIO m
-  => MVar (TaskIndex, Map TaskIndex (IO ()))
+runActiveFinalizers :: forall m.
+     MonadAff m
+  => MVar (TaskIndex, Map TaskIndex (Aff ()))
   -> m ()
 runActiveFinalizers mvar =
   liftEffect $ do
     again <-
-      MVar.modifyMVar mvar $ \original@(minIx, finalizers0) ->
+      MVar.modifyMVar mvar $ \original@(Tuple minIx finalizers0) ->
         case Map.minViewWithKey finalizers0 of
           Nothing ->
-            pure (original, False)
+            pure (Tuple original False)
 
-          Just ((ix, finalize), finalizers) ->
+          Just (Tuple (Tuple ix finalize) finalizers) ->
             if ix == minIx + 1 then do
               finalize
-              pure ((ix, finalizers), True)
+              pure (Tuple (Tuple ix finalizers) True)
             else
-              pure (original, False)
+              pure (Tuple original False)
 
     when again $
       runActiveFinalizers mvar
 
 finalizeTask ::
-     MonadIO m
-  => MVar (TaskIndex, Map TaskIndex (IO ()))
+     MonadAff m
+  => MVar (TaskIndex, Map TaskIndex (Aff ()))
   -> TaskIndex
-  -> IO ()
+  -> Aff ()
   -> m ()
 finalizeTask mvar ix finalize = do
-  liftEffect <<< MVar.modifyMVar_ mvar $ \(minIx, finalizers) ->
-    pure (minIx, Map.insert ix finalize finalizers)
+  liftEffect <<< MVar.modifyMVar_ mvar $ \(Tuple minIx finalizers) ->
+    pure (Tuple minIx (Map.insert ix finalize finalizers))
   runActiveFinalizers mvar
 
 -- | Update the number of capabilities but never set it lower than it already
 --   is.
 --
-updateNumCapabilities :: WorkerCount -> IO ()
+updateNumCapabilities :: WorkerCount -> Aff ()
 updateNumCapabilities (WorkerCount n) = when rtsSupportsBoundThreads $ do
   ncaps <- Conc.getNumCapabilities
   Conc.setNumCapabilities (max n ncaps)
