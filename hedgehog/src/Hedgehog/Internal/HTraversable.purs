72f8c2e347f5bdcbdf64bbc219f89e1688ed91ec
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-04-13 15:21:16 -0700

    Port HTraversale.hs to PS

---
+++ hedgehog/src/Hedgehog/Internal/HTraversable.purs
@@ -0,0 +1,12 @@
+module Hedgehog.Internal.HTraversable
+  ( class HTraversable
+  , htraverse
+  ) where
+
+import Control.Applicative (class Applicative)
+
+-- | Higher-order traversable functors.
+-- |
+-- | This is used internally to make symbolic variables concrete given an 'Environment'.
+class HTraversable t where
+  htraverse :: forall f g h. Applicative f => (forall a. g a -> f (h a)) -> t g -> f (t h)
